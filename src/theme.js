7980b3593a7057ef2b3c3348b64248d36ca0dec2
chintan <chintanrajodiya8098@gmail.com>
2025-04-25 00:49:09 +0530

    razorpay

--- src/theme.js
+++ src/theme.js
@@ -28,6 +28,22 @@ const muiTheme = createMuiTheme({
       dark: '#388e3c',
       contrastText: '#ffffff',
     },
+    grey: {
+      50: '#fafafa',
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+      A100: '#d5d5d5',
+      A200: '#aaaaaa',
+      A400: '#303030',
+      A700: '#616161',
+    },
     background: {
       default: '#ffffff',
       paper: '#ffffff',
@@ -35,6 +51,18 @@ const muiTheme = createMuiTheme({
     text: {
       primary: '#000000',
       secondary: 'rgba(0, 0, 0, 0.6)',
+    },
+    common: {
+      black: '#000000',
+      white: '#ffffff',
+    },
+    action: {
+      active: 'rgba(0, 0, 0, 0.54)',
+      hover: 'rgba(0, 0, 0, 0.04)',
+      selected: 'rgba(0, 0, 0, 0.08)',
+      disabled: 'rgba(0, 0, 0, 0.26)',
+      disabledBackground: 'rgba(0, 0, 0, 0.12)',
+      focus: 'rgba(0, 0, 0, 0.12)',
     },
   },
   components: {
